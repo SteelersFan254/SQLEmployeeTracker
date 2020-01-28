cf7d273b3fbfe59b43eecbc3bc637a8f86cc1026
Ryan Borja <rcborja.school@gmail.com>
2020-01-27 19:13:16 -0600

    finished basic necessities

--- app.js
+++ app.js
@@ -1,51 +1,78 @@
-const inquirer = require("inquirer");
-const mysql = require("mysql");
-
+var mysql = require("mysql");
+var inquirer = require("inquirer");
 var connection = mysql.createConnection({
     host: "localhost",
-
+  
     // Your port; if not 3306
     port: 3306,
-
+  
     // Your username
     user: "root",
-
+  
     // Your password
     password: "password",
     database: "companyDB"
-});
-
-connection.connect(function (err) {
-    if (err) throw err;
-    console.log("connected as id " + connection.threadId);
-    afterConnection()
-});
-
-function afterConnection() {
-    connection.query("SELECT * FROM songs", function (err, res) {
+  });
+  connection.connect(function(err) {});
+  inquirer.prompt([
+    {
+        type: "list",
+        message: "What would you like to do?",
+        name: "firstQuestion",
+        choices: ["Add department", "Add role", "Add employee", "View department", "View roles", "View employees", "Update role", "Delete department", "Delete role", "Delete employee"
+        ]
+    }
+]).then(function (data) {
+    console.log(data)
+    var path = data.firstQuestion
+    switch (path) {
+        case "Add department":
+            return addDepartment();
+        case "Add role":
+            return addRole();
+        case "Add employee":
+            return addEmployee();
+        case "View department":
+            return viewDepartment();
+        case "View roles":
+            return viewRoles();
+        case "View employees":
+            return viewEmployees();
+        case "Update role":
+            return updateEmployee();
+        case "Delete department":
+            return deleteDepartment;
+        case "Delete roll":
+            return deleteRoll;
+        case "Delete employee":
+            return deleteEmployee;
+    }
+})
+function viewEmployees() {
+    console.log("Bringing up employee...\n");
+    connection.query("SELECT * FROM employee", function(err, res) {
         if (err) throw err;
-        console.log(res);
-        connection.end();
+        console.table(res);
+        connection.end()
     });
-}
-
-function createDepartment(data) {
-    console.log("Creating a new department...\n");
-    connection.query("INSERT INTO department SET ?", data);
 };
-
-function createRole(data) {
-    console.log("Creating a new role...\n");
-    connection.query("INSERT INTO role SET ?", data);
+function viewDepartment() {
+    console.log("Bringing up department...\n");
+    connection.query("SELECT * FROM department", function(err, res) {
+        if (err) throw err;
+        console.table(res);
+        connection.end()
+    });
 };
-
---show ALL books with authors
---INNER JOIN will only return all matching values from both tables
-SELECT title, firstName, lastName
-FROM books
-INNER JOIN authors ON books.authorId = authors.id;
-
-function createEmployee() {
+function viewRoles() {
+    console.log("Bringing up employee...\n");
+    connection.query("SELECT * FROM role", function(err, res) {
+        if (err) throw err;
+        console.table(res);
+        connection.end()
+    });
+};
+function addEmployee() {
     inquirer.prompt([
         {
             type: "input",
@@ -61,88 +88,78 @@ function createEmployee() {
             name: "roleID",
         },
     ]).then(function (data) {
-
         console.log("Creating a new employee...\n");
         connection.query("INSERT INTO employee SET ?", data);
+        console.log(data);
+        connection.end()
     })
 };
-
-function seeDepartment(data) {
-    console.log("Bringing up department...\n");
-    connection.query("SELECT * FROM department", function (err, res) {
-        if (err) throw err;
-        console.log(res)
-    });
+// console.table
+// add department
+function addDepartment() {
+    inquirer.prompt([
+        {
+            type: "input",
+            message: "What department would you like to add?",
+            name: "name",
+        }, 
+        ]).then(function (data) {
+        console.log("Creating a new department...\n");
+        connection.query("INSERT INTO department SET ?", data);
+        console.log(data);
+        connection.end()
+    })
 };
-
-function seeRole(data) {
-    console.log("Bringing up role...\n");
-    connection.query("SELECT * FROM role", function (err, res) {
-        if (err) throw err;
-        console.log(res)
-    });
-};
-
-function viewEmployee(data) {
-    console.log("Bringing up employee...\n");
-    connection.query("SELECT * FROM employee;", function (err, res) {
-        if (err) throw err;
-        console.log(res)
-    });
-};
-
-function viewEmployeeByDep() {
-    console.log("Bringing up employees by department...\n");
-    connection.query("SELECT firstName, lastName FROM employee WHERE employee.departmentID = department.ID AND department.name =" + data.name, function (err, res) {
-        if (err) throw err;
-        console.log(res)
-    });
-};
-
-
-
+// add roles
+function addRole() {
+    inquirer.prompt([
+        {
+            type: "input",
+            message: "What role would you like to add?",
+            name: "title"
+        },{
+            type: "input",
+            message: "What is the annual salary of this role?",
+            name: "salary"
+        },{
+            type: "input",
+            message: "Which department ID does the role belong in? (1, 2, or 3)",
+            name: "departmentID"
+        }
+    ]).then(function (data) {
+    console.log("Inserting a new role.../n");
+    connection.query("INSERT INTO role SET ?", data);
+    console.log(data);
+    connection.end()
+})}
+// add employees
+// view (SELECT * FROM) departments
+// view roles
+// view employees
+// update roles 
 function updateEmployee() {
-
     inquirer.prompt([
         {
             type: "input",
             message: "What role would you like to assign?",
             name: "roleID"
-        },{
+        }, {
             type: "input",
-            message: "Which employee would you like to change that role to?",
+            message: "Which employee would you like to change that role to? (First name only)",
             name: "firstName"
         }
-
-    ]).then(function(data) {
-    console.log("Updating role of the empolyee...\n");
-    connection.query("UPDATE employee SET ? WHERE ?", data);
-})
+    ]).then(function (data) {
+        console.log("Updating role of the empolyee...\n");
+        connection.query("UPDATE employee SET ? WHERE ?",
+        [
+            {
+                roleID: data.roleID
+            },
+            {
+                firstName: data.firstName
+            }
+        ]);
+        console.log(data);
+        connection.end()
+    })
 };
-
-// What would you like to do?
-//     View Employees
-//     View Employees by Department
-//       Which department of employees would you like to view?
-//         BOH
-//         FOH
-//         Management
-//     View Employees by Manager
-//       Which managers employees would you like to view?
-//         Manager 1
-//         Manager 2
-//     Add Employee
-//     Remove Employee
-//     Update Employee Manager
-//      Which Employee would you like to assign to a manager?
-//     update employee role
-//       Which employee's role would you like to update?
-//     view departments
-//     view roles
-// Build a command-line application that at a minimum allows the user to:
-
-// Add departments, roles, employees
-
-// View departments, roles, employees
-
-// Update employee roles
